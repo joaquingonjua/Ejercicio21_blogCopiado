e249c3ca0fa6f1eb4bbe990d32879df7b3ba3334
Andrw77 <prietoandi2@gmail.com>
2023-05-08 13:27:45 -0300

    Modify, Edit
    Se ponen en funcionamiento los metodos post y delete.
    Se limita el texto a mostrar del contenido en la tabla de articulos.
    Se agrega columna para imagenes, aun sin rellenar

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,8 +1,14 @@
 const { Article } = require("../models");
+const { format, formatDistance, formatRelative, subDays } = require("date-fns");
+const { es } = require("date-fns/locale");
 
 // Display a listing of the resource.
 async function index(req, res) {
   const articles = await Article.findAll({ include: "author" });
+  for (let i = 0; i < articles.length; i++) {
+    articles[i].createdAt.defaultValue = format(new Date(), "dd MM yyyy HH:mm", { locale: es });
+  }
+  console.log(articles[0].createdAt);
   res.render("admin", { articles });
 }
 
@@ -17,19 +23,40 @@ async function create(req, res) {
 }
 
 // Store a newly created resource in storage.
-async function store(req, res) {}
+async function store(req, res) {
+  await Article.create({
+    title: req.body.title,
+    content: req.body.content,
+    authorId: Number(req.body.authorId),
+  });
+  res.redirect("/articulos");
+}
 
 // Show the form for editing the specified resource.
 async function edit(req, res) {
-  res.render("editArticle");
+  const article = await Article.findByPk(req.params.id);
+  req.body.title = res.render("editArticle", { article });
 }
 
 // Update the specified resource in storage.
-async function update(req, res) {}
+async function update(req, res) {
+  await Article.update(
+    {
+      title: req.body.title,
+      content: req.body.content,
+      authorId: req.body.authorId,
+    },
+    {
+      where: { id: req.params.id },
+    },
+  );
+  console.log(req.body);
+  res.redirect("/articulos");
+}
 
 // Remove the specified resource from storage.
 async function destroy(req, res) {
-  await db.delet("users", req.params.id);
+  await Article.destroy({ where: { id: req.params.id } });
   res.redirect("/articulos");
 }
 
