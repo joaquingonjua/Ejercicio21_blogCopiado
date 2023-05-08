41280a844b5c3bf606ecbad05e90cdd89dff8b9b
augustoriccardi <130772213+augustoriccardi@users.noreply.github.com>
2023-05-08 12:59:04 -0300

    Vista borrador Article
    Maquetado de vista Article.ejs

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -7,7 +7,9 @@ async function index(req, res) {
 }
 
 // Display the specified resource.
-async function show(req, res) {}
+async function show(req, res) {
+  res.render("article");
+}
 
 // Show the form for creating a new resource
 async function create(req, res) {
